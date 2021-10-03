8be6d1cad582bc2bbe0a706c2560b63c5f74f63a
Chris McCormick <chris@mccormick.cx>
2021-10-03 12:51:05 +0800

    Replace core.async with promesa for nbb support.

--- src/sitefox/web.cljs
+++ src/sitefox/web.cljs
@@ -2,6 +2,7 @@
   (:require
     [sitefox.util :refer [env]]
     [sitefox.db :as db]
+    [promesa.core :as p]
     [applied-science.js-interop :as j]
     ["path" :as path]
     ["process" :as process]
@@ -11,31 +12,32 @@
     ["serve-static" :as serve-static]
     ["express-session" :as session]
     ["morgan" :as morgan]
-    ["rotating-file-stream" :as rfs]
-    [cljs.core.async :refer (go <!) :as async]
-    [cljs.core.async.interop :refer-macros [<p!]]))
+    ["rotating-file-stream" :as rfs]))
 
 (def dir (or js/__dirname "./"))
 
 (defn create-store [kv]
   (let [e (session/Store.)]
     (aset e "destroy" (fn [sid callback]
-                        (go (<p! (j/call kv :delete sid))
-                            (when callback
-                              (callback)))))
+                        (p/let [result (j/call kv :delete sid)]
+                          (when callback (callback))
+                          result)))
     (aset e "get" (fn [sid callback]
-                    (go (callback
-                          nil
-                          (<p! (j/call kv :get sid))))))
+                    (p/let [result (j/call kv :get sid)]
+                      (callback nil result)
+                      result)))
     (aset e "set" (fn [sid session callback]
-                    (go (<p! (j/call kv :set sid session))
-                        (callback))))
+                    (p/let [result (j/call kv :set sid session)]
+                      (when callback (callback))
+                      result)))
     (aset e "touch" (fn [sid session callback]
-                    (go (<p! (j/call kv :set sid session))
-                        (callback))))
+                      (p/let [result (j/call kv :set sid session)]
+                        (when callback (callback))
+                        result)))
     (aset e "clear" (fn [callback]
-                      (go (<p! (js/call kv :clear))
-                          (callback))))
+                      (p/let [result (js/call kv :clear)]
+                        (when callback (callback))
+                        result)))
     e))
 
 (defn add-default-middleware [app]
