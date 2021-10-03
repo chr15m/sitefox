e7114ef4dd219b7dee9e8b2262962792f3e8b0de
Chris McCormick <chris@mccormick.cx>
2021-10-04 07:33:36 +0800

    Use Reagent to render the output.

--- examples/nbb/webserver.clj
+++ examples/nbb/webserver.clj
@@ -1,11 +1,12 @@
 (ns webserver
   (:require
     [promesa.core :as p]
-    [sitefox.web :as web]))
+    [sitefox.web :as web]
+    [reagent.dom.server :refer [render-to-static-markup] :rename {render-to-static-markup r}]))
 
 (defn setup-routes [app]
   (web/reset-routes app)
-  (.get app "/" (fn [req res] (.send res "Hello world!"))))
+  (.get app "/" (fn [req res] (.send res (r [:h1 "Hello world!"])))))
 
 (p/let [app (web/create)
         [host port] (web/serve app)]
