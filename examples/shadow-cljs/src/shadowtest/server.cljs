e6c461c4b459d5ef8712e5706865345e1cbbeba3
Chris McCormick <chris@mccormick.cx>
2021-10-05 20:40:44 +0800

    Re-added the reloader code.

--- examples/shadow-cljs/src/shadowtest/server.cljs
+++ examples/shadow-cljs/src/shadowtest/server.cljs
@@ -3,7 +3,7 @@
     [promesa.core :as p]
     [reagent.dom.server :refer [render-to-static-markup] :rename {render-to-static-markup r}]
     [sitefox.web :as web]
-    ;[sitefox.reloader :refer [reloader]]
+    [sitefox.reloader :refer [reloader]]
     [sitefox.logging :refer [bind-console-log-to-file]]))
 
 (bind-console-log-to-file)
@@ -18,7 +18,7 @@
 
 (defn main! []
   (p/let [[app host port] (web/start)]
-    ;(reloader (partial #'setup-routes app))
+    (reloader (partial #'setup-routes app))
     (setup-routes app)
     (println "Server main.")))
 
