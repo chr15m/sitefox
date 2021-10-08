f443a116fa8dcb1a00e41790cd97524b8266c6bc
Michiel Borkent <michielborkent@gmail.com>
2021-10-08 22:36:47 +0200

    Clean up example (for clj-kondo usage)

--- examples/nbb/webserver.clj
+++ examples/nbb/webserver.clj
@@ -1,14 +1,14 @@
 (ns webserver
   (:require
     [promesa.core :as p]
-    [sitefox.web :as web]
-    [reagent.dom.server :refer [render-to-static-markup] :rename {render-to-static-markup r}]))
+    [reagent.dom.server :refer [render-to-static-markup] :rename {render-to-static-markup r}]
+    [sitefox.web :as web]))
 
 (defn setup-routes [app]
   (web/reset-routes app)
-  (.get app "/" (fn [req res] (.send res (r [:h1 "Hello world!"])))))
+  (.get app "/" (fn [_req res] (.send res (r [:h1 "Hello world!"])))))
 
 (p/let [app (web/create)
-        [host port] (web/serve app)]
+        [_host _port] (web/serve app)]
   (setup-routes app)
   (println "Serving."))
