54b9b57a996955e1f6c7a4545b96f336f3ed829d
Chris McCormick <chris@mccormick.cx>
2021-10-09 11:05:33 +0800

    Example of email sending.

---
+++ examples/send-email/README.md
@@ -0,0 +1,10 @@
+Send emails with Sitefox using [nbb](https://github.com/borkdude/nbb).
+
+To test this out:
+
+ 1. Clone this repo.
+ 2. Go into `examples/send-email`.
+ 3. `npm i`
+ 4. `npm run send`
+
+This will run `email-example.clj` using nbb, and you will see the test email results output.
---
+++ examples/send-email/email-example.clj
@@ -0,0 +1,29 @@
+(ns webserver
+  (:require
+    [promesa.core :as p]
+    [reagent.dom.server :refer [render-to-static-markup] :rename {render-to-static-markup r}]
+    [sitefox.mail :as mail]))
+
+(p/do! 
+  (print "Sending a basic text email.")
+  (-> (mail/send-email
+        "chris@mccormick.cx"
+        "test@example.com"
+        "This is my test email."
+        :text "Hello, This is my first email from **Sitefox**. Thank you.")
+      (.then js/console.log))
+
+  (print "Sending with multiple recipients, Reagent HTML, and custom X-Hello header.")
+  (-> (mail/send-email
+        ["chris@mccormick.cx"
+         "goober@goober.com"]
+        "test@example.com"
+        "This is my test email."
+        :text "Hello, This is my second email from **Sitefox**. Thank you."
+        :html (r [:p
+                  [:strong "Hello,"] [:br]
+                  "This is my second email from Sitefox." [:br]
+                  [:strong "Thank you."]])
+        :headers {:X-Hello "Hello world!"})
+      (.then js/console.log)))
+
