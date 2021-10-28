88847cdf5dd674824eb935daaff1c423ed86bf1d
Chris McCormick <chris@mccormick.cx>
2021-10-28 16:27:53 +0800

    Move clj-kondo to dot-prefixed name.

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ if (name) {
   console.log("Creating", name);
   fs.copySync(__dirname + "/template", name);
   fs.moveSync(name + "/gitignore", name + "/.gitignore");
-  fs.moveSync(name + "/clj-kondo", name + "/.clj-kondo");
   replace.sync({
     "files": [
       args[0] + "/**/**",
