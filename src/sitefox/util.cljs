707d201bbf652c76adebbda188c243331644f9ef
Chris McCormick <chris@mccormick.cx>
2021-11-04 20:10:03 +0800

    Documentation.

--- src/sitefox/util.cljs
+++ src/sitefox/util.cljs
@@ -1,18 +1,28 @@
 (ns sitefox.util
+  "Various utilities for server side ClojureScript."
   (:require
     ["json-stringify-safe" :as json-stringify-safe]
     [sitefox.logging :refer [bail]]))
 
-(defn env [k & [default]]
+(defn env
+  "Returns the environment variable named in k with optional default value."
+  [k & [default]]
   (or (aget js/process.env k) default))
 
-(defn env-required [k & [msg]]
+(defn env-required
+  "Returns the environment variable named in k or exits the process if missing.
+  The message printed on exit can be customised in msg."
+  [k & [msg]]
   (or (env k) (bail (or msg (str "Required environment variable is missing:" k)))))
 
-(defn error-to-json [err]
+(defn error-to-json
+  "Convert a JavaScript error into a form that can be returned as JSON data."
+  [err]
   (let [e (js/JSON.parse (json-stringify-safe err))]
     (aset e "message" (str err))
     #js {:error e}))
 
-(defn btoa [s]
+(defn btoa
+  "Server side version of browser JavaScript's btoa base64 encoding."
+  [s]
   (-> s js/Buffer. (.toString "base64")))
