f0def3daa8cc9b6c11c428954fba6ffdb76549f5
Chris McCormick <chris@mccormick.cx>
2021-11-06 16:44:56 +0800

    Updated deps.

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,12 +1,11 @@
 {:npm-deps
  {"rotating-file-stream" "^2.1.6",
   "node-html-parser" "^4.1.5",
+  "json-stringify-safe" "^5.0.1",
   "nodemailer" "^6.6.5",
   "cookie-parser" "^1.4.5",
   "morgan" "^1.10.0",
-  "chokidar" "^3.5.2",
   "sqlite3" "^5.0.2",
-  "caller-id" "^0.1.0",
   "express" "^4.17.1",
   "csurf" "^1.11.0",
   "express-session" "^1.17.2",
