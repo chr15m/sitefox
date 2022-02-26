3f08b7dd44ebd14b685afcd0d356699fb0d33696
Chris McCormick <chris@mccormick.cx>
2022-02-26 16:57:06 +0800

    Continued work on the auth example. #2

--- examples/authentication/src/authexample/server.cljs
+++ examples/authentication/src/authexample/server.cljs
@@ -1,66 +1,146 @@
 (ns authexample.server
   (:require
     ["fs" :as fs]
+    ["node-input-validator" :refer [Validator]]
     [applied-science.js-interop :as j]
     [promesa.core :as p]
     [sitefox.html :refer [render-into]]
     [sitefox.web :as web]
+    [sitefox.ui :refer [log]]
     [sitefox.logging :refer [bind-console-to-file]]))
 
 (bind-console-to-file)
 
 (defonce server (atom nil))
 
-(def template (fs/readFileSync "index.html"))
+;***** views *****;
 
-(defn view:sign-in [csrf-token]
+(defn component-error [errors k]
+  (let [err (aget errors (name k))]
+    (when err [:p.warning (-> err (aget "message"))])))
+
+(defn view:sign-in [csrf-token data errors]
   [:div
    [:h1 "Sign in"]
    [:div.card
     [:form {:method "POST"}
-     [:p [:input.fit {:name "email" :placeholder "Your email"}]]
-     [:p [:input.fit {:name "password" :placeholder "password"}]]
+     [:p [:input.fit {:name "email" :placeholder "Your email" :default-value (aget data "email")}]]
+     [component-error errors :email]
+     [:p [:input.fit {:name "password" :placeholder "password" :default-value (aget data "password")}]]
+     [component-error errors :password]
      [:input {:name "_csrf" :type "hidden" :value csrf-token}]
-     [:div.side-by-side
-      [:span
-       [:a {:href "/sign-up"} "Sign up"]
-       [:br]
-       [:a {:href "/forgot-password"} "Forgot password"]]
+     [:div.actions
+      [:ul
+       [:li [:a {:href "/sign-up"} "Sign up"]]
+       [:li [:a {:href "/forgot-password"} "Forgot password?"]]]
       [:button.primary {:type "submit"} "Sign in"]]]]])
 
-(defn view:sign-up [csrf-token]
+(defn view:sign-up [csrf-token data errors]
   [:div
    [:h1 "Sign up"]
    [:div.card
     [:p "Enter your email to sign up."]
     [:form {:method "POST"}
      [:p [:input.fit {:name "email" :placeholder "Your email"}]]
+     [component-error errors :email]
      [:input {:name "_csrf" :type "hidden" :value csrf-token}]
-     [:div.side-by-side
-      [:span]
-      [:button.primary {:type "submit"} "Sign in"]]]]])
+     [:div.actions
+      [:ul
+       [:li [:a {:href "/sign-in"} "Sign in"]]]
+      [:button.primary {:type "submit"} "Sign up"]]]]])
 
-(defn view:forgot-password [csrf-token])
+(defn view:forgot-password [csrf-token]
+  [:div
+   [:h1 "Sign up"]
+   [:div.card
+    [:p "Enter your email to reset your password."]
+    [:form {:method "POST"}
+     [:p [:input.fit {:name "email" :placeholder "Your email"}]]
+     [:input {:name "_csrf" :type "hidden" :value csrf-token}]
+     [:div.actions
+      [:ul
+       [:li [:a {:href "/sign-in"} "Sign in"]]]
+      [:button.primary {:type "submit"} "Reset"]]]]])
+
+(defn view:error []
+  [:div.warning "The form was tampered with."])
+
+;***** functions *****;
 
 (defn is-post [req]
   (= (aget req "method") "POST"))
 
-(defn serve-form [req res view]
-  (.send res (render-into template "main" [view (j/call req :csrfToken)])))
+(defn serve-form [req res template selector view]
+  (.send res (render-into template selector [view (j/call req :csrfToken)])))
 
-(defn sign-in [req res])
+(defn validate-post-data [req fields warnings]
+  (p/let [data (aget req "body")
+          validator (Validator. data (clj->js fields) (clj->js warnings))
+          validated (.check validator)
+          validation-errors (aget validator "errors")]
+    [data validated validation-errors]))
 
-(defn sign-up [req res])
+(defn check-form [req res template selector view fields & [warnings]]
+  (p/let [is-post (= (aget req "method") "POST")
+          [data validated validation-errors] (when is-post (validate-post-data req fields warnings))
+          passed-validation (and is-post validated)
+          rendered-html (render-into template selector [view (j/call req :csrfToken) (or data #js {}) (or validation-errors #js {})])]
+    (if (or (not is-post)
+            (not passed-validation))
+      (do (.send res rendered-html) nil)
+      data)))
 
-(defn forgot-password [req res])
+;***** handlers *****;
 
-(defn handle-csrf-error [err req res n]
+(defn handle-csrf-error [err _req res n template selector view]
   (if (= (aget err "code") "EBADCSRFTOKEN")
     (-> res
         (.status 403)
-        (.send (render-into template "main" [:div.warning "The form was tampered with."])))
+        (.send (render-into template selector view)))
     (n err)))
 
+(defn sign-in [req res _n template selector view]
+  (p/let [fields {:email ["required" "email"]
+                  :password ["required"]}
+          data (check-form req res template selector view fields)]
+    (when data
+      (log "data:" data)
+      (.send res "VALID"))))
+
+(defn sign-up [req res _n template selector view]
+  (p/let [fields {:email ["required" "email"]}
+          data (check-form req res template selector view fields)]
+    (when data
+      (log "data:" data)
+      (.send res "VALID"))))
+
+(defn forgot-password [req res n template selector view]
+  (case (aget req "method")
+    "GET" (serve-form req res template selector view)
+    "POST" (.send res "Sign in")
+    (n)))
+
+(defn setup-routes [app]
+  (let [template (fs/readFileSync "index.html")]
+    (web/reset-routes app)
+    (web/static-folder app "/css" "node_modules/minimal-stylesheet/")
+    (j/call app :use #(handle-csrf-error %1 %2 %3 %4 template "main" view:error))
+    (j/call app :use "/sign-up" #(sign-up %1 %2 %3 template "main" view:sign-up))
+    (j/call app :use "/forgot-password" #(forgot-password %1 %2 %3 template "main" view:forgot-password))
+    (j/call app :use "/" #(sign-in %1 %2 %3 template "main" view:sign-in))))
+
+(defn main! []
+  (p/let [[app host port] (web/start)]
+    (reset! server app)
+    (setup-routes app)
+    (println "Server started on " (str host ":" port))))
+
+(defn ^:dev/after-load reload []
+  (js/console.log "Reloading")
+  (setup-routes @server))
+
+
+; sending email example
 #_ (-> (mail/send-email
         "test@example.com"
         "test@example.com"
@@ -70,21 +150,3 @@
                 (js/JSON.stringify data nil 2)))
       (.then #(js/console.log "Email: " (aget % "url"))))
 
-(defn setup-routes [app]
-  (web/reset-routes app)
-  (web/static-folder app "/css" "node_modules/minimal-stylesheet/")
-  (j/call app :use handle-csrf-error)
-  (j/call app :get "/" (fn [req res] (serve-form req res view:sign-in)))
-  (j/call app :post "/" sign-in)
-  (j/call app :get "/sign-up" (fn [req res] (serve-form req res view:sign-up)))
-  (j/call app :post "/sign-up" sign-up))
-
-(defn main! []
-  (p/let [[app host port] (web/start)]
-    (reset! server app)
-    (setup-routes app)
-    (println "Server main.")))
-
-(defn ^:dev/after-load reload []
-  (js/console.log "reload")
-  (setup-routes @server))
