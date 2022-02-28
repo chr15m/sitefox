3518d39ae32a57e127bd821fb13bddb124d3e375
Chris McCormick <chris@mccormick.cx>
2022-02-28 21:14:51 +0800

    Authentication sign-up flow emails being sent.

--- examples/authentication/src/authexample/server.cljs
+++ examples/authentication/src/authexample/server.cljs
@@ -1,12 +1,16 @@
 (ns authexample.server
   (:require
     ["fs" :as fs]
+    ["crypto" :refer [createHash createHmac]]
     ["node-input-validator" :refer [Validator]]
+    ["html-to-text" :refer [htmlToText]]
     [applied-science.js-interop :as j]
     [promesa.core :as p]
-    [sitefox.html :refer [render-into]]
+    [sitefox.html :refer [render-into render]]
     [sitefox.web :as web]
+    [sitefox.util :refer [env]]
     [sitefox.ui :refer [log]]
+    [sitefox.mail :refer [send-email]]
     [sitefox.logging :refer [bind-console-to-file]]))
 
 (bind-console-to-file)
@@ -23,10 +27,11 @@
   [:div
    [:h1 "Sign in"]
    [:div.card
+    [:p "Enter your email and password to sign in."]
     [:form {:method "POST"}
      [:p [:input.fit {:name "email" :placeholder "Your email" :default-value (aget data "email")}]]
      [component-error errors :email]
-     [:p [:input.fit {:name "password" :placeholder "password" :default-value (aget data "password")}]]
+     [:p [:input.fit {:name "password" :type "password" :placeholder "password" :default-value (aget data "password")}]]
      [component-error errors :password]
      [:input {:name "_csrf" :type "hidden" :value csrf-token}]
      [:div.actions
@@ -41,13 +46,23 @@
    [:div.card
     [:p "Enter your email to sign up."]
     [:form {:method "POST"}
-     [:p [:input.fit {:name "email" :placeholder "Your email"}]]
+     [:p [:input.fit {:name "email" :placeholder "Your email" :default-value (aget data "email")}]]
      [component-error errors :email]
+     [:p "Verify email:"]
+     [:p [:input.fit {:name "email2" :placeholder "Your email (again)" :default-value (aget data "email2")}]]
+     [component-error errors :email2]
      [:input {:name "_csrf" :type "hidden" :value csrf-token}]
      [:div.actions
       [:ul
        [:li [:a {:href "/sign-in"} "Sign in"]]]
       [:button.primary {:type "submit"} "Sign up"]]]]])
+
+(defn view:sign-up-sent [data]
+  [:div
+   [:h1 "Verification sent"]
+   [:div.card
+    [:p "Thanks for signing up. A verification has been sent to " (aget data "email") "."]
+    [:p "Please check your email and follow the activation link to verify your account."]]])
 
 (defn view:forgot-password [csrf-token]
   [:div
@@ -62,6 +77,13 @@
        [:li [:a {:href "/sign-in"} "Sign in"]]]
       [:button.primary {:type "submit"} "Reset"]]]]])
 
+(defn view:sign-up-email [req verify-url]
+  [:div
+   [:h1 {:align "center"} "Signup verification"]
+   [:p {:align "center"} "Click the link to verify your signup at " (aget req "hostname")]
+   [:p {:align "center"}
+    [:a {:href verify-url} verify-url]]])
+
 (defn view:error []
   [:div.warning "The form was tampered with."])
 
@@ -75,7 +97,7 @@
 
 (defn validate-post-data [req fields warnings]
   (p/let [data (aget req "body")
-          validator (Validator. data (clj->js fields) (clj->js warnings))
+          validator (Validator. data (clj->js fields) (clj->js (or warnings {})))
           validated (.check validator)
           validation-errors (aget validator "errors")]
     [data validated validation-errors]))
@@ -90,6 +112,15 @@
       (do (.send res rendered-html) nil)
       data)))
 
+(defn make-hmac-token [secret size & materials]
+  (let [s (-> (createHash "sha512") (.update secret) .digest)
+        h (createHmac "sha512" s)]
+    (doseq [v materials]
+      (.update h (str v)))
+    (-> h
+        (.digest "hex")
+        (.slice 0 size))))
+
 ;***** handlers *****;
 
 (defn handle-csrf-error [err _req res n template selector view]
@@ -99,6 +130,10 @@
         (.send (render-into template selector view)))
     (n err)))
 
+(defn csrf-handler [template selector view]
+  (fn [err req res n]
+    (handle-csrf-error err req res n template selector view)))
+
 (defn sign-in [req res _n template selector view]
   (p/let [fields {:email ["required" "email"]
                   :password ["required"]}
@@ -107,12 +142,29 @@
       (log "data:" data)
       (.send res "VALID"))))
 
-(defn sign-up [req res _n template selector view]
-  (p/let [fields {:email ["required" "email"]}
-          data (check-form req res template selector view fields)]
+(defn sign-up [req res _n template selector view [view-done email-view email-subject]]
+  (p/let [fields {:email ["required" "email"]
+                  :email2 ["required" "email" "same:email"]}
+          messages {:email2.same "Email addresses must match."}
+          data (check-form req res template selector view fields messages)]
+    ; emit a warning if SECRET is not set
+    (when (nil? (env "SECRET")) (js/console.error "Warning: env var SECRET is not set."))
     (when data
-      (log "data:" data)
-      (.send res "VALID"))))
+      (p/let [time-window (-> (js/Date.) .getTime)
+              token (make-hmac-token (env "SECRET" "DEVMODE") 8 "sign-up" time-window (aget data "email"))
+              verify-url (str (web/build-absolute-uri req "/verify-sign-up") "?h=" token)
+              email-html (render [email-view req verify-url])
+              email-text (htmlToText email-html #js {:hideLinkHrefIfSameAsText true
+                                                     :uppercaseHeadings false})
+              email-subject (or email-subject (str (aget req "hostname") " sign-up verification"))
+              mail-result (send-email (aget data "email")
+                                      (env "FROM_EMAIL" (str "no-reply@" (aget req "hostname")))
+                                      email-subject
+                                      :text email-text
+                                      :html email-html)]
+        (print "mail-result" mail-result)
+        (print "verify-url" verify-url)
+        (.send res (render-into template selector [view-done data]))))))
 
 (defn forgot-password [req res n template selector view]
   (case (aget req "method")
@@ -120,14 +172,31 @@
     "POST" (.send res "Sign in")
     (n)))
 
+(defn change-password [req res n template selector view])
+
+(defn magic-link [req res n template selector view])
+
+(defn auth-view [view-function template selector view & args]
+  (fn [err req res]
+    (view-function err req res template selector view args)))
+
+(defn setup-auth-routes
+  "Add authentication to your app. This function adds /sign-in /sign-up and /forgot-password routes to your app."
+  ([app template]
+   (setup-auth-routes app template "main"))
+  ([app template selector]
+   (j/call app :use "/sign-up" (auth-view sign-up template selector view:sign-up view:sign-up-sent view:sign-up-email))
+   (j/call app :use "/verify-sign-up" (fn []))
+   (j/call app :use "/forgot-password" (auth-view forgot-password template selector view:forgot-password))
+   (j/call app :use "/sign-in" (auth-view sign-in template selector view:sign-in))
+   (j/call app :use "/magic-link" (fn []))))
+
 (defn setup-routes [app]
   (let [template (fs/readFileSync "index.html")]
     (web/reset-routes app)
     (web/static-folder app "/css" "node_modules/minimal-stylesheet/")
-    (j/call app :use #(handle-csrf-error %1 %2 %3 %4 template "main" view:error))
-    (j/call app :use "/sign-up" #(sign-up %1 %2 %3 template "main" view:sign-up))
-    (j/call app :use "/forgot-password" #(forgot-password %1 %2 %3 template "main" view:forgot-password))
-    (j/call app :use "/" #(sign-in %1 %2 %3 template "main" view:sign-in))))
+    (j/call app :use (csrf-handler template "main" view:error))
+    (setup-auth-routes app template "main")))
 
 (defn main! []
   (p/let [[app host port] (web/start)]
@@ -138,15 +207,3 @@
 (defn ^:dev/after-load reload []
   (js/console.log "Reloading")
   (setup-routes @server))
-
-
-; sending email example
-#_ (-> (mail/send-email
-        "test@example.com"
-        "test@example.com"
-        "Form results."
-        :text (str
-                "Here is the result of the form:\n\n"
-                (js/JSON.stringify data nil 2)))
-      (.then #(js/console.log "Email: " (aget % "url"))))
-
